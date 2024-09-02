dd930f3f803ba457370de4e9da24afb8bf6e2e0f
thiagoanalytics <thiagoromualdo.nascimento@gmail.com>
2024-09-02 14:44:52 -0300

    feat: testado a conexão

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import pandas as pd
-import pyodbc as odbc
+import sqlalchemy as sa
 import json
 import os
 from datetime import datetime
 import sys
-
-enable_venv = os.path.join(os.path.dirname(os.path.abspath(__file__)), r'.venv\Scripts\activate_this.py')
-exec(open(enable_venv).read(), {'__file__': enable_venv})
 
 def load_config(json_path):
     """Carrega as configurações do arquivo JSON."""
@@ -15,13 +12,12 @@ def load_config(json_path):
     return config
 
 def get_connection_string(config):
-    """Constrói a string de conexão a partir das configurações."""
+    """Constrói a string de conexão SQLAlchemy a partir das configurações."""
     try:
         conn_str = (
-            f"Driver={{ {config['driver']} }};"
-            f"Server={config['server']};"
-            f"Database={config['database']};"
-            f"Trusted_Connection={config['trusted_connection']};"
+            f"mssql+pyodbc://{config['server']}/{config['database']}"
+            "?driver=SQL+Server+Native+Client+11.0"
+            "&trusted_connection=yes"
         )
         return conn_str
     except KeyError as e:
@@ -39,21 +35,17 @@ def main(json_path):
         output_path = config['output_path']
         file_prefix = config['file_prefix']
         
-        # Constrói a string de conexão
+        # Constrói a string de conexão usando SQLAlchemy
         connection_string = get_connection_string(conn_config)
         
         # Conecta ao banco de dados
-        ConnectionString = odbc.connect(connection_string)
+        engine = sa.create_engine(connection_string)
         
         # Consulta SQL
-        Query = pd.read_sql_query(
-            '''
+        query = '''
             select * from [dbStone].[Auxiliar].[StoneCampanhaCliente]
-            ''', ConnectionString
-        )
-        
-        # Cria DataFrame
-        DF = pd.DataFrame(Query)
+        '''
+        df = pd.read_sql_query(query, engine)
         
         # Define o caminho completo do arquivo CSV
         if not os.path.exists(output_path):
@@ -61,7 +53,7 @@ def main(json_path):
         caminho_completo = os.path.join(output_path, file_prefix + datetime.now().strftime("%Y%m%d") + '.csv')
         
         # Salva o DataFrame como CSV
-        DF.to_csv(caminho_completo, index=False, encoding='utf8')
+        df.to_csv(caminho_completo, index=False, encoding='utf-8-sig')
         print(f'Arquivo CSV salvo em: {caminho_completo}')
     
     except Exception as e:
